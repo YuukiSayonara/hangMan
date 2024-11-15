31568c9f585ce2e3be29cc4262b8ade20e134e41
andrey pautin <andrey.pautin@simbirsoft.com>
2024-11-15 20:34:06 +0300

    find-with-string-array

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,5 @@
 package main;
-
-
-import main.java.FindLetter;
 import main.java.Game;
-import main.java.ListOfWords;
-
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
